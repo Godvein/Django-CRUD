2cbae753fb98420bf37979e5e56f2b33f2da623e
Ankit Dware <sabindware@gmail.com>
2024-10-01 21:03:13 +0545

    new update
    successfully created updatedata view that takes in the request and id
     of the data to update and save the data performing update operation

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('insert/', views.insertData, name = 'insert'),
     path('viewdata/', views.viewData, name = 'viewdata'),
+    path('updatedata/<id>/', views.updateData, name = 'updatedata'),
+    path('deletedata/<id>/', views.deleteData, name = 'deletedata'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Student
 # Create your views here.
 def index(request):
@@ -13,6 +13,30 @@ def insertData(request):
     return render(request, "index.html")    
 
 def viewData(request):
+    
+    studentdata = Student.objects.all()
+    data = {
+        "studentdata" : studentdata
+    }
+    return render(request, "viewdata.html", data)
+
+def updateData(request, id):
+    if request.method == "POST":
+        name = request.POST["Name"]
+        email = request.POST["Email"]
+        edit = Student.objects.get(id = id)
+        edit.name = name
+        edit.email = email
+        edit.save()
+        return redirect("/viewdata/")
+
+    d = Student.objects.get(id = id)
+    data = {
+        "d" : d
+    }
+    return render(request, "updatedata.html", data)
+
+def deleteData(request, id):
     studentdata = Student.objects.all()
     data = {
         "studentdata" : studentdata
