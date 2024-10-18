dbcbcd821bceaaaa45cdddb272faaae8e5e20040
Ankit Dware <sabindware@gmail.com>
2024-10-18 21:43:22 +0545

    security

--- Studentdb/settings.py
+++ Studentdb/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&+3_mx^xeta978+s!m8f$y+h%um+2hf+ypdm6$30scukbh8#=f'
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
