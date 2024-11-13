84085d621dc648e0f3f2a5abaf23f6164a604691
Ankit Dware <sabindware@gmail.com>
2024-11-13 14:51:32 +0545

    added test for api view url

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
+from core.views import ApiListViewCreate, ApiRetrieveUpdateDestroy
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+
+    #test api class view url
+    def test_apilistview_resolve(self):
+        url = reverse('apiview')
+        self.assertEqual(resolve(url).func.view_class, ApiListViewCreate)
